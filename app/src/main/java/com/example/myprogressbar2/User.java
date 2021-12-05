95dfc66fd8863e89d7e2a16fe997602696c5665a
Eve <plescaevelyn@yahoo.com>
2021-12-05 04:11:30 +0200

    dsafasf

---
+++ app/src/main/java/com/example/myprogressbar2/User.java
@@ -0,0 +1,16 @@
+package com.example.myprogressbar2;
+
+public class User {
+    public String fullName;
+    public String email;
+    public String password;
+
+    public User(){
+    }
+
+    public User(String fullname, String email, String password){
+        this.fullName = fullname;
+        this.email = email;
+        this.password = password;
+    }
+}
